4c3016253f6607be18b7049440e01fe3c04516fd
zhoujianpiao <zhoujianpiao@ghzhushou.com>
2022-06-16 14:21:16 +0800

    优化接口

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
-  process.env.NODE_ENV === "prod"
-    ? "http://www.hongrunxingcarpet.com/api"
-    : "http://localhost:7001/api";
+  process.env.NODE_ENV === "dev"
+    ? "http://localhost:7001/api"
+    : "http://www.hongrunxingcarpet.com/api";
